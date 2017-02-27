592ec9a112738d8b113e5e8bfd4883806c2a9334
pieter-1983 <pieter.sterckx@cegeka.com>
2017-02-27 16:19:29 +0100

    solved customer Controller

--- Order/src/main/java/be/cegeka/orders/order/controllers/CustomerController.java
+++ Order/src/main/java/be/cegeka/orders/order/controllers/CustomerController.java
@@ -24,17 +24,21 @@ public class CustomerController {
             @RequestParam(value = "lastName") String lastName,
             @RequestParam(value = "address") String address,
             @RequestParam(value = "eMail") String eMail,
-            @RequestParam(value = "phoneNumber") String phoneNumber){
+            @RequestParam(value = "phoneNumber") String phoneNumber) {
         customerService.addCustomer(firstName, lastName, address, eMail, phoneNumber);
     }
 
-    @RequestMapping(value = "order",method = RequestMethod.POST)
+    @RequestMapping(value = "order", method = RequestMethod.POST)
     @ResponseBody
     public void addItemToOrder(
             @RequestParam(value = "customerID", required = true) int customer_id,
             @RequestParam(value = "item_id") int item_id,
             @RequestParam(value = "quantity") int quantity,
-            @RequestParam(value = "order_id", required = false) int order_id) throws Exception {
-                orderService.createOrderEntry(customer_id, item_id, quantity, order_id);
+            @RequestParam(value = "order_id", required = false) Integer order_id) throws Exception {
+        if (order_id==null) {
+            orderService.createOrderEntry(customer_id, item_id, quantity);
+        } else {
+            orderService.createOrderEntry(customer_id, item_id, quantity, order_id);
+        }
     }
 }
