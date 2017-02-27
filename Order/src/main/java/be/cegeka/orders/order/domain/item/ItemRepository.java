4823e5bf6df8a89d768a11e7c63aca2496f3686b
pieter-1983 <pieter.sterckx@cegeka.com>
2017-02-27 11:06:26 +0100

    fixed bug in admin+added transactionals on repo, tested postman OK

--- Order/src/main/java/be/cegeka/orders/order/domain/item/ItemRepository.java
+++ Order/src/main/java/be/cegeka/orders/order/domain/item/ItemRepository.java
@@ -5,6 +5,7 @@ import be.cegeka.orders.order.domain.customers.Customer;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.util.List;
 
 
@@ -16,7 +17,7 @@ public class ItemRepository {
     public List<Item> getAll(){
         return entityManager.createQuery("select c from Item c" , Item.class).getResultList();
     }
-
+    @Transactional
     public void addItem(Item item) {
         entityManager.persist(item);
     }
